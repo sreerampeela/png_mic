dabbfc17c6682ad91f67e9df239fd45e45c88656
sreerampeela <59723039+sreerampeela@users.noreply.github.com>
2023-05-23 16:20:52 +0530

    Add files via upload

---
+++ automodel.py
@@ -0,0 +1,31 @@
+# The script for fully automated comparative modeling
+# Adapted from MODELLER manual
+
+from modeller import *
+from modeller.automodel import * # Load the AutoModel class
+import argparse
+# Creating arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--query", help="The name of query sequence in the alignment file")
+parser.add_argument("--template", help="The name of template(s) in the alignment file.For separate multiple templates with commas(,)")
+parser.add_argument("--pirfile", help="The name of the alignment file")
+parser.add_argument("--nmodels", help="Number of models to generate")
+args = parser.parse_args() # to parse the arguments
+log.verbose()
+env = Environ()
+
+# directories for input atom files
+env.io.atom_files_directory = ['.', '../atom_files']
+a = LoopModel(env,
+# file with template codes and target sequence
+alnfile = args.pirfile,
+# PDB codes of the templates
+knowns = (args.template.split(",")),
+# code of the target
+sequence = args.query)
+a.auto_align() # get an automatic alignment
+a.md_level = None # No refinement of model
+a.loop.starting_model = 1 # First loop model
+a.loop.ending_model = int(args.nmodels) # Last loop model
+a.loop.md_level = refine.fast # Loop model refinement level
+a.make() # do comparative modeling
